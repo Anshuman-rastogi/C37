46323cda3c491ca13ef4f3f2b210bf9353a2a05b
Anshuman-rastogi <rastogi.ansh26@gmail.com>
2020-12-01 19:52:43 +0530

    play state of the game

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,41 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
       form = new Form()
       form.display();
+    }
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers!==undefined){
+      var displayPos = 130;
+      for(var x in allPlayers){
+        if(x === "player" + player.index)
+          fill("green");
+        else
+          fill("black"); 
+          
+        displayPos += 20;
+        textSize(15);
+        text(allPlayers[x].name + ": " + allPlayers[x].distance, 120, displayPos);
+      }
+
+      if(keyIsDown(UP_ARROW) && player.index!==null){
+        player.distance += 50;
+        player.update();
+      }
     }
   }
 }
